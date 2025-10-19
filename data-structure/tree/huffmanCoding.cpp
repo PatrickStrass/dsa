518086fe0b8ab6346bc2009ebbe32644c4d0aa2c
Patrick dos Santos <patrick.strassburger53@gmail.com>
2025-10-18 22:58:04 -0300

    feat: implements Huffman decoding

--- data-structure/tree/huffmanCoding.cpp
+++ data-structure/tree/huffmanCoding.cpp
@@ -31,14 +31,14 @@ class Compare {
 public:
   bool operator()(Node *a, Node *b) {
     if (a->freq != b->freq)
-      return a->freq > b->freq;   // smaller freq = higher priority
+      return a->freq > b->freq; // smaller freq = higher priority
 
     bool aLeaf = (a->ch != '\0');
     bool bLeaf = (b->ch != '\0');
 
     if (aLeaf != bLeaf)
       return !aLeaf; // prefer leaf nodes over internal ones
-    
+
     if (!aLeaf && !bLeaf) // do not care the order if both are internal nodes
       return true;
 
@@ -48,22 +48,23 @@ public:
 
 // Traverse the tree in preorder manner and push the Huffman code of each
 // character
-void preOrder(Node *root, vector<string> &ans, string curr) {
+void generateCodes(Node *root, unordered_map<char, string> &codes,
+                   string curr) {
   if (!root)
     return;
 
   // Leaf node represents a character.
   if (!root->left && !root->right) {
-    ans.push_back(curr);
+    codes[root->ch] = curr;
 
     return;
   }
 
-  preOrder(root->left, ans, curr + '0');
-  preOrder(root->right, ans, curr + '1');
+  generateCodes(root->left, codes, curr + '0');
+  generateCodes(root->right, codes, curr + '1');
 }
 
-vector<string> encode(string msg) {
+unordered_map<char, string> encode(string msg) {
   // Min heap
   priority_queue<Node *, vector<Node *>, Compare> pq;
   unordered_map<char, int> charFreq;
@@ -92,21 +93,50 @@ vector<string> encode(string msg) {
   }
 
   Node *root = pq.top();
-  vector<string> ans;
-  preOrder(root, ans, "");
+  unordered_map<char, string> codes;
+  generateCodes(root, codes, "");
 
-  return ans;
+  return codes;
+}
+
+string decode(string encoded, unordered_map<char, string> codes) {
+  string decoded = "";
+  string current = "";
+
+  for (char ch : encoded) {
+    current += ch;
+
+    for (auto &pair : codes) {
+      if (pair.second == current) {
+        decoded += pair.first;
+        current = "";
+
+        break;
+      }
+    }
+  }
+
+  return decoded;
 }
 
 int main() {
-  string msg = "paralelepipedo"; // p = 3; a = 2; r = 1; l = 2; e = 3; i = 1; d = 1; o = 1
-  vector<string> ans = encode(msg);
+  string msg = "paralelepipedo";
+  unordered_map<char, string> codes = encode(msg);
+  string encoded = "";
+
+  for (char ch : msg)
+    encoded += codes[ch];
+
+  string decoded = decode(encoded, codes);
 
   cout << "Word: " << msg << endl;
+  cout << "Encoded: " << encoded << endl;
 
-  for (int i = 0; i < ans.size(); i++) {
-    cout << ans[i] << " ";
-  }
+  cout << "Codes:" << endl;
+  for (auto &pair : codes)
+    cout << "'" << pair.first << "': " << "\"" << pair.second << "\"" << endl;
+
+  cout << "Decoded: " << decoded << endl;
 
   return 0;
 }
